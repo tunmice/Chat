190fa06b6e6ed7ca939b1b69fca23d54ea8cc5d8
tunmice <t.otegbayo@gmail.com>
2021-01-03 19:52:12 +0100

    add Storyboard/ViewControllers

---
+++ ChatModule/ChatModule.h
@@ -0,0 +1,18 @@
+//
+//  ChatModule.h
+//  ChatModule
+//
+//  Created by Tunmice on 03/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChatModule.
+FOUNDATION_EXPORT double ChatModuleVersionNumber;
+
+//! Project version string for ChatModule.
+FOUNDATION_EXPORT const unsigned char ChatModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatModule/PublicHeader.h>
+
+
---
+++ ChatModule/ChatViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatViewController.swift
+//  ChatModule
+//
+//  Created by Tunmice on 03/01/2021.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
